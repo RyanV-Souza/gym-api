e10ed4f2ea8c5ec83db8f170130e76a89f04d746
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-30 10:06:11 -0300

    chore: lint the project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
 import { env } from "./env";
-console.log(env)
-app.listen({
-  port: env.PORT,
-  host: '0.0.0.0'
-}).then(() => {
-  console.log('Server listening on port ' + env.PORT)
-})
+
+app
+  .listen({
+    port: env.PORT,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("Server listening on port " + env.PORT);
+  });
