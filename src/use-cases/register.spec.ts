3ca8b96e0d074977f93e4edb6f1bbc28c0915242
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-30 16:21:31 -0300

    refactor: rename UserAlreadyExistsError file

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,7 +2,7 @@ import { expect, describe, it } from "vitest";
 import { RegisterUseCase } from "./register";
 import { compare } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
-import { UserAlreadyExistsError } from "./errors/user-already-exists";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 describe("register use case", () => {
   it("should be able to register", async () => {
