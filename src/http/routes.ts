d933db740941699a7290c7c23ac5f56c620a2485
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-31 12:41:17 -0300

    feat: implements a middleware to verify jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
 import { register } from "./controllers/register";
 import { FastifyInstance } from "fastify";
+import { verifyJwt } from "./middleware/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/user", register);
   app.post("/session", authenticate);
+
+  // Authenticated routes
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
 }
