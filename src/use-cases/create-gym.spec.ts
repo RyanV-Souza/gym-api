272317eeafbaf481a4a5a477070016d945d3e528
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-31 09:43:14 -0300

    test: add CreateGymUseCase

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,27 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { CreateGymUseCase } from "./create-gym";
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: CreateGymUseCase;
+
+describe("create gym use case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CreateGymUseCase(gymsRepository);
+  });
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "Gym 1",
+      description: "Description 1",
+      phone: "123456789",
+      latitude: -23.55,
+      longitude: -46.66,
+    });
+
+    console.log(gym);
+
+    expect(gym.id).toBeDefined();
+  });
+});
