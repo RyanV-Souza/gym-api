412212cec25789cfa74c971b42fa3aa5c2dc57b3
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-30 22:26:30 -0300

    feat: verify if check in is in the same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,14 +1,27 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe("check in use case", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository);
+
+    gymsRepository.items.push({
+      id: "1",
+      title: "Gym 1",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+      phone: "+551199999999",
+      description: null,
+    });
 
     vi.useFakeTimers();
   });
@@ -21,6 +34,8 @@ describe("check in use case", () => {
     const { checkIn } = await sut.execute({
       userId: "1",
       gymId: "1",
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     expect(checkIn.id).toBeDefined();
@@ -32,13 +47,39 @@ describe("check in use case", () => {
     await sut.execute({
       userId: "1",
       gymId: "1",
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     await expect(
       sut.execute({
         userId: "1",
         gymId: "1",
+        userLatitude: 0,
+        userLongitude: 0,
       }),
     ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should not be able to check in twice but in different days", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 8, 0, 0));
+
+    await sut.execute({
+      userId: "1",
+      gymId: "1",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    vi.setSystemTime(new Date(2023, 0, 2, 8, 0, 0));
+
+    await expect(
+      sut.execute({
+        userId: "1",
+        gymId: "1",
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).resolves.toBeDefined();
   });
 });
