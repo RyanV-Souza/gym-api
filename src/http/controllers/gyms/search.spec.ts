8f16d32a8424641f88d8bca53dcf83380ebcbaa6
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-02 11:57:11 -0300

    test: search gym controller e2e

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,53 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Search Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to search gyms", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Gym 1",
+        description: "Description 1",
+        phone: "123456789",
+        latitude: -23.55,
+        longitude: -46.66,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Gym 2",
+        description: "Description 2",
+        phone: "123456789",
+        latitude: -23.55,
+        longitude: -46.66,
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/search")
+      .set("Authorization", `Bearer ${token}`)
+      .query({ q: "1" });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "Gym 1",
+      }),
+    ]);
+  });
+});
