ab59da2d551c8b069c76fc8b726ef90d22529e94
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-02 10:58:09 -0300

    feat: generate a new url schema to a e2e environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest/environments";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("oi");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("tearing down");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
